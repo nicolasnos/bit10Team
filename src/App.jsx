cb558e62565d16dae2d697a338461757aa8ed1a8
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-03-01 17:33:52 -0500

    lista de tarjetas -primera forma

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
+import List from "./Components/List";
 
 function App() {
-  const apiG = async () => {
-    try {
-      const resp = await fetch(
-        "https://api.breakingbadquotes.xyz/v1/quotes/10"
-      );
-      console.log(await resp.json());
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    apiG();
-  }, []);
-
   return (
     <div className="App">
       <h1>Hola</h1>
-      <p>primer commit</p>
+      <List />
     </div>
   );
 }
---
+++ src/Components/List.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
+
+const List = () => {
+  const [data, setData] = useState(null);
+  const [book, setBook] = useState(null);
+
+  const [load, setLoad] = useState(null);
+  const [error, setError] = useState(false);
+
+  //vigila a la api
+  useEffect(() => {
+    showApi();
+  }, []);
+
+  //vigila que si api camba de color, active la funcion de card
+  useEffect(() => {
+    if (data) {
+      showBook();
+      console.log(data.results);
+    }
+  }, [data]);
+
+  //funcion que trae api
+  const showApi = async () => {
+    try {
+      setLoad(true);
+      const res = await fetch("https://gutendex.com/books/?");
+      setData(await res.json());
+    } catch (error) {
+      setError(true);
+    } finally {
+    }
+  };
+
+  //funcion que maneja la informacion para hacerla una card
+  const showBook = () => {
+    const arr = data.results.map((item) => {
+      return (
+        <Card key={item.id} style={{ width: "18rem" }}>
+          <Card.Img variant="top" src={item.formats.image} />
+          <Card.Body>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              {item.authors[0].name}
+            </Card.Subtitle>
+            <Card.Text>{item.subjects[0]}</Card.Text>
+            <Button variant="primary">Boton de editar</Button>
+            <Button variant="danger">Boton de borrar</Button>
+          </Card.Body>
+        </Card>
+      );
+    });
+    setBook(arr);
+  };
+
+  return <section>{book}</section>;
+};
+
+export default List;
+
+{
+  /* pendiente consultar la ruta de la imagen */
+}
