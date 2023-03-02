e85deba8557889f9d59bebf14510f4cba3b0e85d
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-03-01 20:21:29 -0500

    estructura de navegacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./App.css";
-import List from "./Components/List";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
-      <List />
+      <Header />
     </div>
   );
 }
