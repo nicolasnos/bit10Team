ed54100ba61f33d8d28e16e7ec48317484ecb2e0
sharon_monroy <mancotatiana21@gmail.com>
2023-03-03 02:10:59 -0500

    se arregan estilos modal y organiza las targetas

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
+import "../css/List.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Banner from "./Banner";
 import Spinner from "react-bootstrap/Spinner";
 import Alert from "react-bootstrap/Alert";
-import "../css/List.css";
 import { ModalShow } from "./ModalShow";
 
 const List = () => {
@@ -71,7 +71,7 @@ const List = () => {
 
     const arr = filteredBooks.map((item) => {
       return (
-        <Card key={item.id} style={{ width: "18rem" }}>
+        <Card key={item.id} style={{ width: "18rem" }} className="card">
           {" "}
           <Card.Img
             variant="top"
@@ -94,44 +94,51 @@ const List = () => {
   };
 
   return (
-    <section>
-      <Banner />
-      <ModalShow
-        addBook={addBook}
-        setaddBook={setAddBook}
-        newBook={newBook}
-        setNewBook={setNewBook}
-        setBook={setBook}
-      />
-      <div>
-        <input
-          value={search}
-          onChange={searcherBook}
-          type="text"
-          placeholder="Search"
-          className="form-control"
-        />
-      </div>
-      <div>
-        <input
-          value={authorFilter}
-          onChange={authorFilterHandler}
-          type="text"
-          placeholder="Filter by author"
-          className="form-control"
-        />
-      </div>
-      <div>
-        <input
-          value={genreFilter}
-          onChange={genreFilterHandler}
-          type="text"
-          placeholder="Filter by Genero"
-          className="form-control"
-        />
-      </div>
-      {book}
-    </section>
+    <>
+      <Banner />{" "}
+      <section className="contenedor-main">
+        <article>
+          {" "}
+          <div>
+            <input
+              value={search}
+              onChange={searcherBook}
+              type="text"
+              placeholder="Search"
+              className="form-control"
+            />
+          </div>
+          <div>
+            <input
+              value={authorFilter}
+              onChange={authorFilterHandler}
+              type="text"
+              placeholder="Filter by author"
+              className="form-control"
+            />
+          </div>
+          <div>
+            <input
+              value={genreFilter}
+              onChange={genreFilterHandler}
+              type="text"
+              placeholder="Filter by Genero"
+              className="form-control"
+            />
+          </div>
+        </article>
+        <article>
+          <ModalShow
+            addBook={addBook}
+            setaddBook={setAddBook}
+            newBook={newBook}
+            setNewBook={setNewBook}
+            setBook={setBook}
+          />
+        </article>
+        <article className="card-contenedor">{book}</article>
+      </section>
+    </>
   );
 };
 export default List;
