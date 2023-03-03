3b1b8eac746cd89b3a659423f9583c25b083830f
sharon_monroy <mancotatiana21@gmail.com>
2023-03-03 18:14:07 -0500

    se agrega funcionalidad  editar y agregar

--- src/Components/BookIteam.jsx
+++ src/Components/BookIteam.jsx
@@ -11,12 +11,86 @@ export default function BookIteam({
   editItem,
   setEditItem,
 }) {
-  //Fucncion que elimina la tarjeta creada
+  const [newTitle, setNewTitle] = useState(title);
+  const [newAuthor, setNewAuthor] = useState(author);
+  const [newGenre, setNewGenre] = useState(gender);
+
+  //Fucncion que elimina la tarjeta 
   const handleDelete = () => {
     const updatedBookList = [...newBook];
     updatedBookList.splice(index, 1);
     setNewBook(updatedBookList);
   };
+
+  //Funcion que editar la tarjeta 
+
+  const handleEdit = () => {
+    setEditItem({ index, title, author, gender });
+  };
+
+  const handleUpdate = () => {
+    const updatedList = newBook.map((item, i) => {
+      if (i === index) {
+        return { title: newTitle, author: newAuthor, gender: newGenre };
+      }
+      return item;
+    });
+    setNewBook(updatedList);
+    setEditItem({});
+  };
+
+//Funcion que cancelar el editar
+
+  const handleCancel = () => {
+    setNewTitle(title);
+    setNewAuthor(author);
+    setNewGenre(gender);
+    setEditItem({});
+  };
+
+  if (editItem.index === index) {
+    return (
+      <>
+        <Card.Title>
+          <input
+            className="inputEdit"
+            type="text"
+            value={newTitle}
+            id="title-input"
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+        </Card.Title>
+        <Card.Subtitle>
+          <input
+            className="inputEdit"
+            type="text"
+            value={newAuthor}
+            id="author-input"
+            onChange={(e) => setNewAuthor(e.target.value)}
+          />
+        </Card.Subtitle>
+        <Card.Text>
+          <input
+            className="inputEdit"
+            type="text"
+            value={newGenre}
+            id="genre-input"
+            onChange={(e) => setNewGenre(e.target.value)}
+          />
+        </Card.Text>
+        <div>
+          <Button className="btn" variant="success" onClick={handleUpdate}>
+            guardar
+          </Button>{" "}
+          <Button className="btn" variant="danger" onClick={handleCancel}>
+            {" "}
+            cancelar
+          </Button>{" "}
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Card.Title>{title}</Card.Title>
