27fcdc3de978da0794e5b7dfe14af597c6cd7037
Nicolas Olmos <nicolas.nos@hotmail.com>
2023-03-03 19:17:47 -0500

    resolviendo conflictos

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,15 +1,21 @@
 import React, { useState, useEffect } from "react";
+import "../css/List.css";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Banner from "./Banner";
 import Spinner from "react-bootstrap/Spinner";
 import Alert from "react-bootstrap/Alert";
-import "../css/List.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { ModalShow } from "./ModalShow";
 
 const List = () => {
   const [data, setData] = useState(null);
   const [book, setBook] = useState(null);
+  const [addBook, setAddBook] = useState({
+    title: "",
+    author: "",
+    gender: "",
+  });
+  const [newBook, setNewBook] = useState([]);
 
   const [load, setLoad] = useState(null);
   const [error, setError] = useState(false);
@@ -65,7 +71,8 @@ const List = () => {
 
     const arr = filteredBooks.map((item) => {
       return (
-        <Card key={item.id} style={{ width: "18rem" }}>
+        <Card key={item.id} style={{ width: "18rem" }} className="card">
+          {" "}
           <Card.Img
             variant="top"
             src={item.formats + "image/jpeg"}
@@ -87,37 +94,51 @@ const List = () => {
   };
 
   return (
-    <section>
-      <Banner />
-      <div>
-        <input
-          value={search}
-          onChange={searcherBook}
-          type="text"
-          placeholder="Search"
-          className="form-control"
-        />
-      </div>
-      <div>
-        <input
-          value={authorFilter}
-          onChange={authorFilterHandler}
-          type="text"
-          placeholder="Filter by author"
-          className="form-control"
-        />
-      </div>
-      <div>
-        <input
-          value={genreFilter}
-          onChange={genreFilterHandler}
-          type="text"
-          placeholder="Filter by Genero"
-          className="form-control"
-        />
-      </div>
-      {book}
-    </section>
+    <>
+      <Banner />{" "}
+      <section className="contenedor-main">
+        <article>
+          {" "}
+          <div>
+            <input
+              value={search}
+              onChange={searcherBook}
+              type="text"
+              placeholder="Search"
+              className="form-control"
+            />
+          </div>
+          <div>
+            <input
+              value={authorFilter}
+              onChange={authorFilterHandler}
+              type="text"
+              placeholder="Filter by author"
+              className="form-control"
+            />
+          </div>
+          <div>
+            <input
+              value={genreFilter}
+              onChange={genreFilterHandler}
+              type="text"
+              placeholder="Filter by Genero"
+              className="form-control"
+            />
+          </div>
+        </article>
+        <article>
+          <ModalShow
+            addBook={addBook}
+            setaddBook={setAddBook}
+            newBook={newBook}
+            setNewBook={setNewBook}
+            setBook={setBook}
+          />
+        </article>
+        <article className="card-contenedor">{book}</article>
+      </section>
+    </>
   );
 };
 export default List;
