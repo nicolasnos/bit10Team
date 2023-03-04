b8be08fe12e8360989c4a80856360020479d94c7
sharon_monroy <mancotatiana21@gmail.com>
2023-03-04 00:02:06 -0500

    cambiando estilos header y team

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Team = () => {
-  return <div>El mejor Team</div>;
+  return (
+    <>
+      <section>
+        <h1>SOBRE NOSOTROS</h1>
+        <p>
+          Como desarrolladores, somos expertos en programación y diseño web,
+          capaces de crear soluciones de software personalizadas para satisfacer
+          las necesidades específicas de los clientes. Siempre enfocados en la
+          calidad y la eficiencia, podemos resolver problemas técnicos complejos
+          y cumplir con los plazos establecidos para los proyectos. Además,
+          nuestras habilidades de comunicación y trabajo en equipo aseguran una
+          contribución significativa a cualquier proyecto en el que
+          participemos.
+        </p>
+      </section>
+      <section></section>
+    </>
+  );
 };
 
 export default Team;
